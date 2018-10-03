d0466406209179e765c85143324fc8dcba8d3d9b
kwiecien <k.d.kwiecinski@gmail.com>
2018-10-03 22:15:16 +0200

    fix: Upgrade Firebase base SDK for Cloud Functions

--- FriendlyChatFunctions/functions/index.js
+++ FriendlyChatFunctions/functions/index.js
@@ -8,8 +8,9 @@ const functions = require('firebase-functions');
 exports.emojify = // !!! cloud function name !!!
   functions.database // !!! service !!!
   .ref('/messages/{pushId}/text') // !!! conditions !!!
-  .onWrite(event => { // !!! event handler !!!
-    // Database write events include new, modified, or deleted database nodes. // All three types of events at the specific database path trigger this
+  .onWrite((change, context) => { // !!! event handler !!!
+    // Database write events include new, modified, or deleted database nodes.
+    // All three types of events at the specific database path trigger this
     // cloud function.
     // For this function we only want to emojify new database nodes, so we'll
     // first check to exit out of the function early if this isn't a new
@@ -17,7 +18,9 @@ exports.emojify = // !!! cloud function name !!!
 
     // !event.data.val() is a deleted event
     // event.data.previous.val() is a modified event
-    if (!event.data.val() || event.data.previous.val()) {
+    console.log(change.after.val())
+    console.log(change.before.val())
+    if (!change.after.val() || change.before.val()) {
       console.log("not a new write event")
       return
     }
@@ -26,11 +29,11 @@ exports.emojify = // !!! cloud function name !!!
     console.log("emojifying!")
 
     // Get the value from the 'text' key of the message
-    const originalText = event.data.val()
+    const originalText = change.after.val()
     const emojifiedText = emojifyText(originalText)
 
     // Return a JavaScript Promise to update the database node
-    return event.data.ref.set(emojifyText)
+    return change.after.ref.set(emojifiedText)
   });
 
 // Returns text with keywords replaced by emoji
